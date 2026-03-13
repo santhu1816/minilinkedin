404a30b4363ee93e448718e11bbd9642690adb18
santhu1816 <playersanthosh29@gmail.com>
2026-03-13 14:59:59 +0530

    Delete admin.js

--- admin.js
+++
@@ -1,9 +0,0 @@
-const adminMiddleware = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(403).json({ error: 'Access denied. Admin privileges required.' });
-  }
-};
-
-module.exports = adminMiddleware;
